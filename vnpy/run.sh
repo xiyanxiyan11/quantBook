6e02fc408edac1bb41aaf95eb6dd99f11d5f0f04
meihongwei <xiyanxiyan10@hotmail.com>
2018-09-19 14:03:34 +0800

    add mongo in dockerfile

--- vnpy/run.sh
+++ vnpy/run.sh
@@ -1,1 +1,2 @@
-docker run -it -v /Users/xiyanxiyan10/project/vnpy/:/mnt -p 9876:9876 vnpy /bin/bash
+docker run -it -v /Users/xiyanxiyan10/project/vnpy/:/vnpy -p 9876:80 -p 5900:5900 vnpy-vnc /bin/bash
+
